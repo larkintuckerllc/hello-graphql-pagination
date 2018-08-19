0594463b2d24e8593963a4f45fff46584a6fe633
John Tucker <john.tucker@sharpspring.com>
2018-08-19 05:35:08 -0400

    Hello World

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import cors from 'cors';
 import express from 'express';
+
 const app = express();
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.use(cors());
+app.use((_, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  return next();
+});
+const typeDefs = gql`
+  type Query {
+    "A simple type for getting started!"
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'world',
+  },
+};
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
